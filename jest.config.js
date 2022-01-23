c9e1d408ebee98fd40f89260c3af3abba6214d00
Bruno Coitinho <bruno.coitinho@mppb.mp.br>
2022-01-23 06:56:09 -0300

    fixing coverage, tsconfig, prettier

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,25 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-export default {
+module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  rootDir: 'src',
+
+  // transform: {
+  //   '^.+\\.jsx?$': 'babel-jest', // Adding this line solved the issue
+  //   '^.+\\.tsx?$': 'ts-jest',
+  // },
+
+  // setupFilesAfterEnv: [
+  //   'jest-enzyme',
+  //   './jest.setup.js',
+  // ],
+
+  // testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+  verbose: false, // this shows each test inside describes in output
 
   collectCoverage: true,
-  collectCoverageFrom: ['<rootDir>/src/services/**/*.ts'],
+  collectCoverageFrom: ['!**/src/**'],
   coverageDirectory: 'coverage',
   coverageReporters: ['text-summary', 'lcov'],
+  testPathIgnorePatterns: ['/node_modules/'],
 };
